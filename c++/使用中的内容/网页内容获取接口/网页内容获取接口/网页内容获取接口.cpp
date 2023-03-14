20839d87f06c341e0f96178e1ac4513ad0706f9e
永恒·天心 <38283354+yonghengtianxin@users.noreply.github.com>
2023-03-14 17:10:44 +0800

    把网页内容获取的中文支持写了一下

--- c++/使用中的内容/网页内容获取接口/网页内容获取接口/网页内容获取接口.cpp
+++ c++/使用中的内容/网页内容获取接口/网页内容获取接口/网页内容获取接口.cpp
@@ -1,39 +1,34 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <string>
+#include <shlwapi.h>
+#pragma comment(lib,"Shlwapi.lib") 
 using namespace std;
 int main()
 {
     char* get = getenv("QUERY_STRING");
-	string getstr = get;
 	string str;
-	int pos = 0;
+/*	int pos = 0;
 	int strpos = 0;
 	int i = 0;
 	char chs[2] = { 0 };
 	char* p = nullptr;
 	char ret = 0;
+*/
 
-	if (getstr.find("%") != getstr.npos)
-	{
-		while ((pos = getstr.find("%", pos)) != string::npos)
-		{
-			if (i == 1)
-			{
-				chs[0] = strpos + 1;
-				chs[1] = pos - 1;
-				ret = strtol(chs, &p, 16);
-				getstr.erase(strpos,3);
-				getstr[strpos] = ret;
-				i = 0;
-			}
-			else
-			{
-				strpos = pos;
-				i++;
-			}
-			pos++;
-		}
+	char* 返回的字符串 = new char[strlen(get)+1];
+	char* 传入的字符串 = new char[strlen(get)+1];
+	strcpy(传入的字符串,get);
+	DWORD 字符串长度 = strlen(get)+1;
+	
+	UrlUnescapeA(传入的字符串, 返回的字符串, &字符串长度, NULL);
+
+	string getstr = 返回的字符串;
+
+	if (getstr.find(".") != string::npos) {
+		cout << "Content-type:text/html;charset=GB18030\r\n\r\n";
+		cout << "<p>你不觉得有什么问题吗</p>\n";
+		return 0;
 	}
 
 	ifstream fs(".\\text\\"+getstr);
@@ -51,5 +46,8 @@ int main()
 		cout << "Content-type:text/html;charset=GB18030\r\n\r\n";
 		cout << "<p>找不到指定的内容</p>\n";
 		cout << "<p>请求内容为" << getstr << "</p>\n";
+		cout << "<p>get请求原始内容：" << get << "</p>\n";
+		cout << "<p>解码传入的字符串：" << 传入的字符串 << "</p>\n";
+		cout << "<p>解码返回的字符串" << 返回的字符串 << "</p>\n";
 	}
 }
